4687d831ceaaf49bc9eefa7686557f7790171b16
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-01 01:51:31 +0300

    add logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import { AiOutlineClose } from 'react-icons/ai';
 import logo from '../../images/logo.png';
 
 const Navbar = () => {
-  return <nav className='w-full flex md:justify-center justify-between items-center p-4'>
-
-  </nav>;
+  return (
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+      <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+        <img src={logo} className='w-32 cursor-pointer' alt='logo' />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
