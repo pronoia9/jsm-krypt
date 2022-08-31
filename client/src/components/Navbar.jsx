a83e281fa7f51076422acca35ce1c3af799a670d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-01 01:49:57 +0300

    import requirements

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,11 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import logo from '../../images/logo.png';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+
+  </nav>;
 };
 
 export default Navbar;
