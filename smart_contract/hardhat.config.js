f9f1122114648cd115f91c42b4f363893958c474
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-02 01:59:13 +0300

    update hardhat config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/Qlj2ZnIp1oDeP8bnJDIDJ6Ifwl9D4t4e',
+      accounts: ['833bd8b5926cb4b1f438765b0a1430b7273093c87bfdc340a269e4c43ea17f38'],
+    },
+  },
 };
