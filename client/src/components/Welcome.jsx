9fceb14623cfa1d8aee2811c69da3e830703d28c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-01 23:51:07 +0300

    add input component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,15 +3,14 @@ import { BsInfoCircle } from 'react-icons/bs';
 import { SiEthereum } from 'react-icons/si';
 
 import { Loader } from './';
-const companyCommonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
+const companyCommonStyles =
+  'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
-const Input = (props) => (
-  <input className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism' step='0.0001' {...props} />
-);
+const Input = (props) => (<input className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism' step='0.0001' onChange={(e) => props.handleChange(e, props.name)} {...props} />);
 
 const Welcome = () => {
-  const connectWallet = () => { }
-  
+  const connectWallet = () => {};
+
   return (
     <div className='flex w-full justify-center items-center'>
       <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4"'>
