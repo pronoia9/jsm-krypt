41c78f581a41a614f6b56877f760db818a4b5f2e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-03 02:07:55 +0300

    map dummy data

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,6 +3,10 @@ import { useTransactionContext } from '../contexts/TransactionContext';
 import dummyData from '../utils/dummyData';
 import { sliceAddress } from '../utils/sliceAddress';
 
+const TransactionsCard = ({ id, url, message, timestamp, addressFrom, amount, addressTo }) => (
+  <div className='bg-[#181918] m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] min-w-full flex-col p-3 rounded-md hover:shadow-2xl'></div>
+);
+
 const Transactions = () => {
   const { currentAccount } = useTransactionContext();
 
@@ -15,7 +19,11 @@ const Transactions = () => {
           <h3 className='text-white text-3xl text-center my-2'>Connect your account to see the latest transactions</h3>
         )}
 
-        <div className='flex flex-wrap justify-center items-center mt-10'></div>
+        <div className='flex flex-wrap justify-center items-center mt-10'>
+          {dummyData.reverse().map((item, i) => (
+            <TransactionsCard key={i} {...item} />
+          ))}
+        </div>
       </div>
     </div>
   );
