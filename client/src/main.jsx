fe01fd8c099dd5423853753394af8b016eaa26a0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-02 03:12:37 +0300

    add TransactionProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import App from './App';
+import { TransactionProvider } from './contexts/TransactionContext';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <TransactionProvider>
+    <App />
+  </TransactionProvider>,
+  document.getElementById('root')
+);
