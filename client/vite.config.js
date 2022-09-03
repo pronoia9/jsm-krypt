c4ec8ab028b4d01701750a5b95d83233b3654dae
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-03 03:58:31 +0300

    add base url

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: 'https://pronoia9.github.io/jsm-krypt/',
+  plugins: [react()],
+});
